ab2b3ae438b1a81c2614b5b2dab1607f8f93c466
Fawzia Masud <fmasud11@gmail.com>
2022-10-04 22:22:58 -0400

    html build recipe page

--- sign_in_ui_verification.py
+++ sign_in_ui_verification.py
@@ -10,8 +10,9 @@ driver.get("https://www.amazon.com/")
 driver.find_element(By.XPATH, "//a[@class='nav-a nav-a-2   nav-progressive-attribute']").click()
 
 expected_result = 'Sign in', 'Email or mobile phone number'
-actual_result = driver.find_element(By.XPATH, "//h1[@class= 'a-spacing-small']").text, driver.find_element(By.XPATH, "//div[@class='a-row a-spacing-base']").text
 
+actual_result = driver.find_element(By.XPATH, "//h1[@class= 'a-spacing-small']").text, \
+                driver.find_element(By.XPATH, "//div[@class='a-row a-spacing-base']").text
 assert expected_result == actual_result, f'Error: Expected {expected_result}, but got {actual_result}'
 
 driver.quit()

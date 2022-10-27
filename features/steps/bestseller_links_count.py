84ee3e3bb766268c412ba0992dc015cf2d271e5b
Fawzia Masud <fmasud11@gmail.com>
2022-10-27 03:03:52 -0400

    verify link name and title

--- features/steps/bestseller_links_count.py
+++ features/steps/bestseller_links_count.py
@@ -4,6 +4,8 @@ from behave import given, when, then
 
 
 BESTSELLER_LINKS = (By.CSS_SELECTOR, '._p13n-zg-nav-tab-all_style_zg-tabs__EYPLq li')
+HEADER_LINKS = (By. XPATH, '//div[contains(@class, "nav-tab-all")]//li')
+LINK_TITLES = (By. XPATH, '//span[contains(@class, "LandingPageBannerText")]')
 
 
 @when("Open Amazon BestSellers page")
@@ -15,3 +17,11 @@ def open_bestsellers_page(context):
 def links_on_page(context):
     links = context.driver.find_elements(*BESTSELLER_LINKS)
     assert len(links) == 5, "Expected 5 links but got {len(links}"
+
+
+@then('Clicks on each top link and verifies that correct page opens')
+def header_links(context):
+    all_links = context.driver.find_elements(*HEADER_LINKS)
+    for links in all_links:
+        title = links.find_element(*LINK_TITLES)
+        assert title, 'Error, link titles not found'
--- features/tests/verify_bestseller_links_count.feature
+++ features/tests/verify_bestseller_links_count.feature
@@ -1,6 +1,10 @@
 # Created by ledad at 10/5/2022
-Feature: BestSells links count
+Feature: BestSellers page verification
+#
+#  Scenario: User sees 5 links on BestSellers page
+#    When Open Amazon BestSellers page
+#    Then User sees 5 links on page
 
-  Scenario: User sees 5 links on BestSellers page
+  Scenario: Verify correct page is opened under each header
     When Open Amazon BestSellers page
-    Then User sees 5 links on page
+    Then Clicks on each top link and verifies that correct page opens

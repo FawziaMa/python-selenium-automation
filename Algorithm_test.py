d577176f985be48c9cb14c3421fc84745c77aeca
Fawzia Masud <fmasud11@gmail.com>
2022-11-10 20:39:26 -0500

    Even First and Increment Algorithm HW

--- Algorithm_test.py
+++ Algorithm_test.py
@@ -106,30 +106,66 @@
 # Example: [1, 3, 5, 6, 4, 10, 2, 3] (The arithmetical mean is 4.25),
 # Return [1, 3, 4, 2, 3]
 
-def arthmetical_mean(list):
-    list = [1, 3, 5, 6, 4, 10, 2, 3]
-    list_mean = (sum(list)/len(list))
-    print(list_mean)
-    list2 = []
-    for num in list:
-        if num < list_mean:
-            list2.append(num)
-    return list2
-print(arthmetical_mean(list))
+# def arthmetical_mean(list):
+#     list = [1, 3, 5, 6, 4, 10, 2, 3]
+#     list_mean = (sum(list)/len(list))
+#     print(list_mean)
+#     list2 = []
+#     for num in list:
+#         if num < list_mean:
+#             list2.append(num)
+#     return list2
+# print(arthmetical_mean(list))
+#
+#
+# #PROBLEM 7
+# # Two Lowest Elements
+# # When given a list of elements, find the two lowest elements. They can be equal to each other or different.
+# # Example: [198, 3, 4, 9, 10, 9, 2], Return: 2, 3
+#
+# def lowest_elements(list):
+#     list = [198, 3, 4, 9, 10, 9, 2]
+#     list.sort()
+#     return list[0:2]
+#
+# print(lowest_elements(list))
 
 
-#PROBLEM 7
-# Two Lowest Elements
-# When given a list of elements, find the two lowest elements. They can be equal to each other or different.
-# Example: [198, 3, 4, 9, 10, 9, 2], Return: 2, 3
+# PROBLEM 8
+# Even First
+# Your input is a list of integers, and you have to reorder its entries so that the even entries appear first.
+# You are required to solve it without allocating additional storage (operate with the input list).
+# Example: [7, 3, 5, 6, 4, 10, 3, 2] Return [6, 4, 10, 2, 7, 3, 5, 3]
 
-def lowest_elements(list):
-    list = [198, 3, 4, 9, 10, 9, 2]
-    list.sort()
-    return list[0:2]
+def even_odd(A):
+    even = 0
+    odd = len(A) - 1
+    while even < odd:
+        if A[even] % 2 == 0:
+            even += 1
+        else:
+            A[even], A[odd] = A[odd], A[even]
+            odd -= 1
+    return A
+A = [7, 3, 5, 6, 4, 10, 3, 2]
+print(even_odd(A))
 
-print(lowest_elements(list))
+# PROBLEM 9
+# Increment a Number
+# Write a program that takes as input a list of digits encoding a nonnegative decimal integer D and
+# updates the list to represent the integer D + 1.
+# For example, if the input is [1, 2, 9] then you should update the array to [1, 3, 0].
 
-
+# def increment(S):
+#     if S[-1] == 9:
+#         S[-1] = 0
+#         S[1] += 1
+#     elif S[-1] < 9:
+#         S[-1] = S[-1] + 1
+#     else:
+#         S[0] = S[0] + 1
+#     return S
+# S = [9,9,8]
+# print(increment(S))
 
 
